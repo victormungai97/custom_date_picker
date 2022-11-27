a296b2152001b9c5df580ecd9cece99b8f2b00ea
Victor Kamau <victormungai97@gmail.com>
2022-11-27 18:18:31 +0300

    Initial commit

---
+++ android/app/src/main/java/com/realtimeinnovations/custom_date_picker/custom_date_picker/MainActivity.java
@@ -0,0 +1,6 @@
+package com.realtimeinnovations.custom_date_picker.custom_date_picker;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
